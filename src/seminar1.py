aebefb01b27fd71fed8ac03e4d0afc62e891efe4
Aziz <208465@edu.fa.ru>
2023-09-10 18:27:28 +0300

    solved tasks

--- src/seminar1.py
+++ src/seminar1.py
@@ -11,7 +11,7 @@ def random_matrix(n: int) -> np.array:
     :param n: matrix size
     :return: random n x n matrix
     """
-    return np.array([0])
+    return np.array(np.random.randint(0, 256, size=(n, n, 3), dtype=np.uint8))
 
 
 def broadcast_array(a: np.array, n: int) -> np.array:
@@ -21,7 +21,7 @@ def broadcast_array(a: np.array, n: int) -> np.array:
     :param n: number of rows in output matrix
     :return: 2D matrix
     """
-    return np.ones(n)
+    return np.tile(a, reps=(n, 1))
 
 
 def inplace_operation(a: np.array, b: np.array) -> None:
@@ -31,7 +31,9 @@ def inplace_operation(a: np.array, b: np.array) -> None:
     :param b: matrix B
     :return: None
     """
-
+    a += b
+    a *= -0.5
+    return None
 
 def get_elements(a: np.array, indices: np.array) -> np.array:
     """
@@ -49,7 +51,7 @@ def get_elements(a: np.array, indices: np.array) -> np.array:
     :param indices: 1D array
     :return: 1D array of elements
     """
-    return a
+    return a[np.arange(len(indices)), indices]
 
 
 def self_inners(a: np.array) -> np.array:
@@ -59,4 +61,4 @@ def self_inners(a: np.array) -> np.array:
     :param a:
     :return: 2D array of inners product shape=(m, m)
     """
-    return a
+    return np.dot(a, a.T)
