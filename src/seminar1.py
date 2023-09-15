5e33b5f1f7d1bfa678ac62a46b0eeaa159c5c703
Alfa <alfa@HNBA108088.local>
2023-09-16 01:28:02 +0300

    done 1st homework

--- src/seminar1.py
+++ src/seminar1.py
@@ -21,7 +21,7 @@ def broadcast_array(a: np.array, n: int) -> np.array:
     :param n: number of rows in output matrix
     :return: 2D matrix
     """
-    return np.ones(n)
+    return np.tile(a, (n, 1))
 
 
 def inplace_operation(a: np.array, b: np.array) -> None:
@@ -31,10 +31,10 @@ def inplace_operation(a: np.array, b: np.array) -> None:
     :param b: matrix B
     :return: None
     """
-    a += b
-    a *= -1
-    a /= 2
-
+    np.add(a, b, out=b)
+    np.negative(a, out=a)
+    np.divide(a, 2, out=a)
+    np.matmul(b, a, out=a)
 
 def get_elements(a: np.array, indices: np.array) -> np.array:
     """
@@ -52,7 +52,7 @@ def get_elements(a: np.array, indices: np.array) -> np.array:
     :param indices: 1D array
     :return: 1D array of elements
     """
-    return a
+    return a[np.arange(a.shape[0]), indices]
 
 
 def self_inners(a: np.array) -> np.array:
@@ -62,4 +62,4 @@ def self_inners(a: np.array) -> np.array:
     :param a:
     :return: 2D array of inners product shape=(m, m)
     """
-    return a
+    return np.dot(a, a.T)
