660ab625b1aa3e10a30b4144ba7bc0fe76cf6919
Alina Merkulova <merrkl@mail.ru>
2023-09-16 15:55:21 +0300

    FirstVersionMerk

--- src/seminar1.py
+++ src/seminar1.py
@@ -3,16 +3,16 @@
 
 import numpy as np
 
-
 def random_matrix(n: int) -> np.array:
     """
     Make (n x n x 3) matrix with a random uniform distribution [0, 255]
     array type must be uint8
     :param n: matrix size
-    :return: random n x n x 3 matrix
+    :return: random n x n matrix
     """
-    return np.random.randint(0, 255, size=(n, n, 3), dtype=np.uint8)
-
+    random_values = np.random.uniform(low=0, high=255, size=(n, n, 3))
+    matrx = random_values.astype(np.uint8)
+    return matrx
 
 def broadcast_array(a: np.array, n: int) -> np.array:
     """
@@ -21,8 +21,7 @@ def broadcast_array(a: np.array, n: int) -> np.array:
     :param n: number of rows in output matrix
     :return: 2D matrix
     """
-    return np.ones(n)
-
+    return np.tile(a, (n, 1))
 
 def inplace_operation(a: np.array, b: np.array) -> None:
     """
@@ -31,10 +30,7 @@ def inplace_operation(a: np.array, b: np.array) -> None:
     :param b: matrix B
     :return: None
     """
-    a += b
-    a *= -1
-    a /= 2
-
+    np.multiply(np.add(a, b), np.divide(a, -2), out = a)
 
 def get_elements(a: np.array, indices: np.array) -> np.array:
     """
@@ -52,7 +48,7 @@ def get_elements(a: np.array, indices: np.array) -> np.array:
     :param indices: 1D array
     :return: 1D array of elements
     """
-    return a
+    return np.array([a[row, indices[row]] for row in range(a.shape[0])])
 
 
 def self_inners(a: np.array) -> np.array:
@@ -62,4 +58,4 @@ def self_inners(a: np.array) -> np.array:
     :param a:
     :return: 2D array of inners product shape=(m, m)
     """
-    return a
+    return np.inner(a, a)
