3c8ed61f312188d2fd7f973e91341586d0bd62b2
Vlada <Vlada_stikharnaya@mail.ru>
2023-10-03 18:43:58 +0300

    Test

--- src/test.py
+++ src/test.py
@@ -2,10 +2,7 @@ import numpy as np
 
 
 def main():
-    x = np.stack([np.ones(5), np.arange(5)])
-    e_x = np.exp(x)
-    sm = np.sum(e_x, axis=1, keepdims=True)
-    print(e_x/sm)
+    print(np.zeros_like(np.ones((3073, 2)) * 1e-3))
 
 
 if __name__ == '__main__':
