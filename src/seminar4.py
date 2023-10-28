7e96b48c5d862a5c209e2021be8422feb481662b
AJlEXANDR <s.aleks-02@mail.ru>
2023-10-28 14:43:15 +0300

    Fix seminar 4

--- src/seminar4.py
+++ src/seminar4.py
@@ -34,7 +34,9 @@ class Optimizer(ABC):
 class SGD(Optimizer):
     def step(self, w, d_w, learning_rate):
         # TODO Update W with d_W
-        pass
+        # DONE
+        w -= d_w * learning_rate
+        # pass
 
 
 class Momentum(Optimizer):
@@ -46,13 +48,20 @@ class Momentum(Optimizer):
         if self.velocity is None:
             self.velocity = np.zeros_like(d_w)
         # TODO Update W with d_W and velocity
+        # DONE
+        new_velocity = self.rho * self.velocity + d_w
+        w -= new_velocity * learning_rate
+        self.velocity = new_velocity
 
 
 class DropoutLayer(Layer):
     def forward(self, x: np.ndarray, train: bool = True) -> np.ndarray:
         if train:
             # TODO zero mask in random X position and scale remains
-            pass
+            # DONE
+            self.mask = np.random.random(size=x.shape) > self.p
+            self.scale = 1/(1 - self.p)
+            return x * self.scale * self.mask
         else:
             return x
 
@@ -103,10 +112,15 @@ class BatchNormLayer(Layer):
         self.num_examples = x.shape[0]
         if train:
             # TODO Compute mean_x and var_x
+            # DONE
+            self.mean_x = np.mean(x, axis=0, keepdims=True)
+            self.var_x = np.var(x, axis=0, keepdims=True)
             self._update_running_variables()
         else:
             # TODO Copy mean_x and var_x from running variables
-            pass
+            # DONE
+            self.mean_x = self.running_mean_x
+            self.var_x = self.running_var_x
 
         self.var_x += epsilon
         self.stddev_x = np.sqrt(self.var_x)
@@ -175,7 +189,8 @@ class NeuralNetwork:
         num_classes = np.max(y) + 1
         loss_history = []
         for it in range(num_iters):
-            idxs = np.random.choice(num_classes, batch_size)
+            # idxs = np.random.choice(num_classes, batch_size)
+            idxs = np.random.choice(len(X), batch_size)
             X_batch, y_batch = X[idxs], y[idxs]
             # evaluate loss and gradient
             z = self.forward(X_batch)
@@ -183,7 +198,6 @@ class NeuralNetwork:
             self.backward()
 
             # Update gradients
-
             for name, param in self.params().items():
                 self.optimizers[name].step(param.value, param.grad, learning_rate)
 
@@ -202,14 +216,57 @@ class NeuralNetwork:
                 model_params[f'layer_{i}_{k}'] = v
         return model_params
 
+    def evaluate(self, X, y):
+        z = self.forward(X)
+        y_predicted = np.argmax(z, axis=1)
+        accuracy = np.mean(y_predicted == y)
+        return accuracy
 
-if __name__ == '__main__':
-    """1 point"""
+
+def train():
     (x_train, y_train), (x_test, y_test) = get_preprocessed_data(include_bias=False)
-    # Train your neural net!
     n_input, n_output, n_hidden = 3072, 10, 256
     neural_net = NeuralNetwork([DenseLayer(n_input, n_hidden),
                                 DropoutLayer(0.5),
                                 BatchNormLayer(n_hidden),
                                 ReLULayer(),
                                 DenseLayer(n_hidden, n_output)])
+
+    learning_rate = 0.01
+    num_iters = 1000
+    batch_size = 256
+    reg = 0.0
+
+    neural_net.setup_optimizer(Momentum())
+
+    t0 = datetime.datetime.now()
+    loss_history = neural_net.fit(x_train, y_train,
+                                  learning_rate=learning_rate, num_iters=num_iters,
+                                  batch_size=batch_size, verbose=True)
+    t1 = datetime.datetime.now()
+    dt = t1 - t0
+
+    report = f"""\n# Training Softmax classifier  
+    datetime: {t1.isoformat(' ', 'seconds')}  
+    Well done in: {dt.seconds} seconds  
+    learning_rate = {learning_rate}  
+    reg = {reg}  
+    num_iters = {num_iters}  
+    batch_size = {batch_size}  
+
+    Final loss: {loss_history[-1]}   
+    Train accuracy: {neural_net.evaluate(x_train, y_train)}   
+    Test accuracy: {neural_net.evaluate(x_test, y_test)}  
+    """
+
+    print(report)
+
+    out_dir = 'C:\\Users\\sanha\\PycharmProjects\\neuralnets\\output\\seminar4\\'
+    with open(out_dir + 'report.md', 'w') as f:
+        f.write(report)
+
+
+if __name__ == '__main__':
+    """1 point"""
+    # Train your neural net!
+    train()
