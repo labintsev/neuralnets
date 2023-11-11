727f140e91894010ff0282f3a6533ca12935eaf8
AJlEXANDR <s.aleks-02@mail.ru>
2023-11-11 11:03:27 +0300

    Homework 5 done

--- src/conv_demo_test.py
+++ src/conv_demo_test.py
@@ -4,10 +4,13 @@ import tensorflow as tf
 
 class ConvolutionDemo(unittest.TestCase):
     def setUp(self) -> None:
-        input_shape = (1, 32, 32, 3)
+        # input_shape = (1, 32, 32, 3)
+        input_shape = (1, 10, 10, 3)
         self.x = tf.ones(input_shape)
 
     def test_conv_1d_1(self):
+        # kernel_size - width pf vector
+        # (1, 32, 32, 3) == (1, 32, 32-(kernel_size=3)+1, (filters=5)) == (1, 32, 30, 5)
         conv_1d_layer = tf.keras.layers.Conv1D(filters=5, kernel_size=3)
         z = conv_1d_layer(self.x)
         self.assertEqual(z.shape, (1, 32, 30, 5))
@@ -21,6 +24,9 @@ class ConvolutionDemo(unittest.TestCase):
         conv_1d_layer = tf.keras.layers.Conv2D(filters=5, kernel_size=(3, 3))
         z = conv_1d_layer(self.x)
         self.assertEqual(z.shape, (1, 30, 30, 5))
+        # conv_1d_layer = tf.keras.layers.Conv2D(filters=2, kernel_size=(9, 9))
+        # z = conv_1d_layer(self.x)
+        # self.assertEqual(z.shape, (1, 2, 2, 2))
 
     def test_conv_2d_2(self):
         conv_1d_layer = tf.keras.layers.Conv2D(filters=7, kernel_size=(10, 10))
--- src/seminar5.py
+++ src/seminar5.py
@@ -13,7 +13,7 @@ def build_conv_layer() -> tf.keras.layers.Conv2D:
     :return: keras convolutional layer
     """
     # TODO Create layer with necessary filters, kernel size and striding step
-    my_layer = None
+    my_layer = tf.keras.layers.Conv2D(filters=2, kernel_size=(5, 5), strides=(5, 5))
 
     return my_layer
 
@@ -30,7 +30,7 @@ def build_padded_conv_layer(kernel_size) -> tf.keras.layers.Conv2D:
     """
 
     # TODO Create layer with necessary filters and padding. Kernel size is builder parameter.
-    my_layer = None
+    my_layer = tf.keras.layers.Conv2D(filters=2, kernel_size=kernel_size, padding="same")
 
     return my_layer
 
@@ -39,7 +39,7 @@ def build_depth_wise_conv_layer() -> tf.keras.layers.DepthwiseConv2D:
     """Build DepthWise Convolution layer """
 
     # TODO Create layer with necessary kernel size and depth multiplier
-    my_layer = None
+    my_layer = tf.keras.layers.DepthwiseConv2D(kernel_size=(3, 3), depth_multiplier=2)
     return my_layer
 
 
@@ -47,7 +47,7 @@ def build_pooling_layer() -> tf.keras.layers.MaxPooling2D:
     """Build MaxPooling layer with fixed pool and strides"""
 
     # TODO Create layer with necessary kernel size and strides
-    my_layer = None
+    my_layer = tf.keras.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))
     return my_layer
 
 
@@ -55,5 +55,5 @@ def build_up_conv_layer() -> tf.keras.layers.Conv2DTranspose:
     """Build Transpose Convolution layer"""
 
     # TODO Create layer with necessary filters, kernel size and strides
-    my_layer = None
+    my_layer = tf.keras.layers.Conv2DTranspose(filters=4, kernel_size=(3, 3), strides=(2, 2))
     return my_layer
