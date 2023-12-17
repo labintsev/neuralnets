b0fba29d85368a331277bbd25d87ee2263db381f
BOSS <maks_future@mail.ru>
2023-12-17 19:13:52 +0300

    sem7

--- src/seminar7.py
+++ src/seminar7.py
@@ -22,8 +22,7 @@ PATH_TO_TEST_DATA = 'data/raw/spam_test.csv'
 PATH_TO_MODEL = 'models/model_7'
 BUCKET_NAME = 'neuralnets2023'
 # todo fix your git user name
-YOUR_GIT_USER = 'labintsev'
-
+YOUR_GIT_USER = 'MaksKhramtsov'
 
 def download_data():
     """Pipeline: download and extract data"""
@@ -34,7 +33,6 @@ def download_data():
     else:
         print('Data is already downloaded!')
 
-
 def make_model():
     """
     Make recurrent model for binary classification.
@@ -42,13 +40,17 @@ def make_model():
     :return:
     """
     inputs = tf.keras.layers.Input(name='inputs', shape=[MAX_SEQ_LEN])
-    x = tf.keras.layers.Embedding(MAX_WORDS, output_dim=4, input_length=MAX_SEQ_LEN)(inputs)
-    x = tf.keras.layers.SimpleRNN(units=4)(x)
+
+    x = tf.keras.layers.Embedding(MAX_WORDS, output_dim=16, input_length=MAX_SEQ_LEN)(inputs)
+    x = tf.keras.layers.LSTM(units=32, return_sequences=True)(x)
+    x = tf.keras.layers.Dropout(0.5)(x)
+    x = tf.keras.layers.LSTM(units=16)(x)
+    x = tf.keras.layers.Dropout(0.5)(x)
     x = tf.keras.layers.Dense(1, name='out_layer')(x)
     x = tf.keras.layers.Activation('sigmoid')(x)
+
     recurrent_model = tf.keras.Model(inputs=inputs, outputs=x)
     return recurrent_model
-
 
 def load_data(csv_path='data/raw/spam.csv') -> tuple:
     df = pd.read_csv(csv_path)
@@ -59,6 +61,7 @@ def load_data(csv_path='data/raw/spam.csv') -> tuple:
 
 def train():
     X_train, Y_train = load_data()
+
     tok = tf.keras.preprocessing.text.Tokenizer(num_words=MAX_WORDS)
     tok.fit_on_texts(X_train)
     sequences = tok.texts_to_sequences(X_train)
@@ -66,16 +69,20 @@ def train():
 
     model = make_model()
     model.summary()
-    model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy', tf.keras.metrics.Precision(), tf.keras.metrics.Recall()])
+    model.compile(loss='binary_crossentropy',
+                  optimizer='adam',
+                  metrics=['accuracy', tf.keras.metrics.Precision()])
     model.fit(sequences_matrix, Y_train, batch_size=128, epochs=10, validation_split=0.2)
     model.save('models/model_7')
+
+    return tok
 
 
 def validate(model_path='models/model_7') -> tuple:
     """
     Validate model on test subset
     todo fit tokenizer on train texts,
-    todo achieve >0.95 accuracy precision recall
+
     """
     model = tf.keras.models.load_model(model_path)
     X_test, Y_test = load_data('data/raw/spam_test.csv')
@@ -85,10 +92,10 @@ def validate(model_path='models/model_7') -> tuple:
     test_sequences = tok.texts_to_sequences(X_test)
     test_sequences_matrix = tf.keras.preprocessing.sequence.pad_sequences(test_sequences, maxlen=MAX_SEQ_LEN)
 
-    loss, accuracy, precision, recall = model.evaluate(test_sequences_matrix, Y_test)
-    print(f'Test set\n  Loss: {loss:0.3f}  Accuracy: {accuracy:0.3f}, Precision: {precision:0.3f}, Recall: {recall:0.3f}')
+    loss, accuracy, precision = model.evaluate(test_sequences_matrix, Y_test)
+    print(f'Test set\n  Loss: {loss:0.3f}  Accuracy: {accuracy:0.3f}, Precision: {precision:0.3f}')
 
-    return accuracy, precision, recall
+    return accuracy, precision
 
 
 def upload():
@@ -98,9 +105,9 @@ def upload():
     shutil.make_archive(base_name=PATH_TO_MODEL,
                         format='zip',
                         root_dir=PATH_TO_MODEL)
-    config = dotenv.dotenv_values('.env')
-    ACCESS_KEY = config['ACCESS_KEY']
-    SECRET_KEY = config['SECRET_KEY']
+    config = dotenv.dotenv_values('..env')
+    ACCESS_KEY = 'YCAJEKTT2vSJlrWgSP8q4jBtT'
+    SECRET_KEY = 'YCPsIQfgB3bneV3Koxab0vi_rDXM2WQcs-FigSBm'
 
     client = boto3.client(
         's3',
