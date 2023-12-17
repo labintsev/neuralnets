be02961a3ac74bed335c9a9297e52443243353ab
BOSS <maks_future@mail.ru>
2023-12-17 18:01:37 +0300

    sem6_1

--- src/seminar6.py
+++ src/seminar6.py
@@ -4,6 +4,8 @@ import os
 import zipfile
 import shutil
 from urllib.request import urlretrieve
+import keras
+from keras import layers
 
 import tensorflow as tf
 import boto3
@@ -35,14 +37,80 @@ def download_data():
 
 
 def make_model(input_shape, num_classes):
-    model = None
-    return model
+    inputs = keras.Input(shape=input_shape)
+
+    # Entry block
+    x = layers.Rescaling(1.0 / 255)(inputs)
+    x = layers.Conv2D(128, 3, strides=2, padding="same")(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+
+    previous_block_activation = x  # Set aside residual
+
+    for size in [256, 512, 728]:
+        x = layers.Activation("relu")(x)
+        x = layers.SeparableConv2D(size, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.Activation("relu")(x)
+        x = layers.SeparableConv2D(size, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.MaxPooling2D(3, strides=2, padding="same")(x)
+
+        # Project residual
+        residual = layers.Conv2D(size, 1, strides=2, padding="same")(
+            previous_block_activation
+        )
+        x = layers.add([x, residual])  # Add back residual
+        previous_block_activation = x  # Set aside next residual
+
+    x = layers.SeparableConv2D(1024, 3, padding="same")(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+
+    x = layers.GlobalAveragePooling2D()(x)
+    if num_classes == 2:
+        units = 1
+    else:
+        units = num_classes
+
+    x = layers.Dropout(0.25)(x)
+    # We specify activation=None so as to return logits
+    outputs = layers.Dense(units, activation=None)(x)
+    return keras.Model(inputs, outputs)
 
 
 def train():
     """Pipeline: Build, train and save model to models/model_6"""
     # Todo: Copy some code from seminar5 and https://keras.io/examples/vision/image_classification_from_scratch/
     print('Training model')
+
+    image_size = (180, 180)
+    batch_size = 128
+
+    train_ds, val_ds = keras.utils.image_dataset_from_directory(
+        "./data/raw/cats_dogs_train/PetImages",
+        validation_split=0.2,
+        subset="both",
+        seed=1337,
+        image_size=image_size,
+        batch_size=batch_size,
+    )
+
+    model = make_model(input_shape=image_size + (3,), num_classes=2)
+    epochs = 1
+    model.compile(
+        optimizer=keras.optimizers.Adam(3e-4),
+        loss=keras.losses.BinaryCrossentropy(from_logits=True),
+        metrics=[keras.metrics.BinaryAccuracy(name="acc")],
+    )
+    model.fit(
+        train_ds,
+        epochs=epochs,
+        validation_data=val_ds,
+    )
+    model.save(PATH_TO_MODEL)
 
 
 def upload():
